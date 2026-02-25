80ecb724369d13f86995c8cdd0573cb12e71c9e7
Kennedy Kubiak <kennedy.kubiak@gmail.com>
2026-02-25 10:14:40 -0800

    added fastapi import to main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from wedding-planner!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# def main():
+   # print("Hello from wedding-planner!")
+
+
+# if __name__ == "__main__":
+    # main()
