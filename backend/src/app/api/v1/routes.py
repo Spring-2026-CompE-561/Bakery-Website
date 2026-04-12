c8a6138a97939b0aa554f4c253b46ce761be4033
Doaa alazraqi <dalazraqi8771@sdsu.edu>
2026-04-12 13:40:42 -0700

    adding routes for about page

--- backend/src/app/api/v1/routes.py
+++ backend/src/app/api/v1/routes.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from fastapi import APIRouter
 
-from ...routes import order, product, user
+from ...routes import order, product, user, about
 
 api_router = APIRouter()
 
@@ -10,3 +10,4 @@ api_router = APIRouter()
 api_router.include_router(product.router, prefix="/products", tags=["Products"])
 api_router.include_router(order.router, prefix="/orders", tags=["Orders"])
 api_router.include_router(user.router, prefix="/user", tags=["User"])
+api_router.include_router(about.router, prefix="/about", tags=["Public"])
