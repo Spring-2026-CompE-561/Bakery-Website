d4ba8ac86ac83701b8467d50ff6957e256937275
bluemeaniez <rjlim@rocketmail.com>
2026-04-22 17:00:52 -0700

    feat: placeholder for menu

--- frontend/data/menu.ts
+++ frontend/data/menu.ts
@@ -16,5 +16,71 @@ export const products = [
         name: "Turon Mini Cake",
         price: "$10",
         image: "/public/globe.svg",
+    },
+    {
+        id: 4,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 5,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 6,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 7,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 8,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 9,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 10,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 11,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 12,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 13,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
+    },
+    {
+        id: 14,
+        name: "Turon Mini Cake",
+        price: "$10",
+        image: "/public/globe.svg",
     }
 ]
